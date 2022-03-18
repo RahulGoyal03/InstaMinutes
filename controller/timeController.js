bc3bda314842f08d70805121433ea535c8af4fec
RahulGoyal03 <91531231+RahulGoyal03@users.noreply.github.com>
2022-03-18 13:47:46 +0530

    updated

---
+++ controller/timeController.js
@@ -0,0 +1,48 @@
+const moment = require("moment");
+
+const triggerFunction = async (req, res) => {
+  try {
+    
+    // importing the reqquestbody
+
+    let eventArray = req.body.eventQueue;
+
+    // if body is empty
+    if (!eventArray) {
+      return res
+        .status(400)
+        .send({ status: "failure", msg: "input the time events" });
+    }
+
+    // going through every event
+    for (let i = 0; i < eventArray.length; i++) {
+      const givenDate = moment(new Date(`${eventArray[i].dateTime}`)).format(
+        "Y-M-D H:m:s"
+      );
+
+      // make sure the date is not from the past
+      if(givenDate < moment().format("Y-M-D H:m:s")){
+        return res.status(400).send({status:false, msg:"enter the valid date not the past date"})
+      }
+
+      // how to get the delay between current time and inputted time
+      const date = new Date(givenDate);
+      const currentDate = new Date();
+      let diff = date - currentDate;
+      let delayTime = diff + eventArray[i].text.length * 1000;
+
+      // consoling the output after the delay
+      setTimeout(() => {
+        console.log(`waiting for ${eventArray[i].text.length} seconds`);
+        console.log(eventArray[i].text.split("").reverse().join(""));
+      }, delayTime);
+    }
+
+    res.status(201).send({ status: "success", msg: "event initiated" });
+
+  } catch (err) {
+    res.status(500).send({ status: "failure", err: err.message });
+  }
+};
+
+module.exports.triggerFunction = triggerFunction;
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const route = require("./routes/route");
+
+app.use("/", route);
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express app running on port " + (process.env.PORT || 3000));
+});
