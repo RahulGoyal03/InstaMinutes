0190d5d8d367e8efe43d51904a5460da90f4f1d6
RahulGoyal03 <91531231+RahulGoyal03@users.noreply.github.com>
2022-03-18 14:55:23 +0530

    updated

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-const express = require("express");
+const express = require("express"); // require express
+const path = require("path"); // require path 
 
-const app = express();
+const app = express(); // run express and store the methods in app
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+let Port = process.env.PORT || 3000 // initalised thr port
 
-const route = require("./routes/route");
+app.use(express.json()); //this is to accept data in json format
 
-app.use("/", route);
+app.use(express.urlencoded({ extended: true })); // this is basically to decode the data send through html form
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Express app running on port " + (process.env.PORT || 3000));
+// const route = require("./routes/route"); 
+
+const triggerFunction = require("../controller/timeController");
+// console.log(timeController)
+
+
+//give the path to our html page and join it with express
+const staticPath = path.join(__dirname, "./public")
+app.use(express.static(staticPath))
+
+app.get("/", (req,res) =>{
+    res.send("hello")
+})
+
+
+app.post("/form", triggerFunction, (req,res) =>{
+    var item = req.body.name;
+    console.log(item);
+})
+
+
+// this is basically to listen on the port
+app.listen(Port, function () {
+  console.log(`App running on port ${Port}` );
 });
